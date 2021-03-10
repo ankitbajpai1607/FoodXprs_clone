24354b011e0146217d16907226d662aa40c5ef0d
nil patil <nilp7744@gmail.com>
2021-03-10 13:33:32 +0530

    page2-nil

---
+++ page2nil.js
@@ -0,0 +1,54 @@
+var h = ["https://i.postimg.cc/jdHG363D/fourth.png","https://i.postimg.cc/5tVjdmbD/second.png", "https://i.postimg.cc/1XCy8GxR/sixxxx.jpg"]
+
+
+
+
+
+
+
+var i = 0;
+
+function nilop() {
+
+
+
+
+
+    document.getElementById("images11").src = h[i];
+
+    if (i < (h.length - 1)) {
+        i++;
+
+
+    }
+    else {
+        i = 0;
+    }
+
+}
+
+function currentSlide() {
+
+
+    document.getElementById("images11").src = h[0];
+
+}
+function currentSlide2() {
+
+
+    document.getElementById("images11").src = h[1];
+
+}
+function currentSlide3() {
+
+
+    document.getElementById("images11").src = h[2];
+
+}
+
+var seet = setInterval(nilop, 2000)
+
+function stop1() {
+    clearInterval(seet)
+}
+
