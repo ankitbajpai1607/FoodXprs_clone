c14015ffaa8d0e6468d257a8118e63d87951a25e
Bhargav1224 <bhargavkudala@gmail.com>
2021-03-11 21:20:43 +0530

    created plan my meal page

--- order-online.js
+++ order-online.js
@@ -1,10 +1,30 @@
+var cust_name;
+var select_location;
+var select_food;
+var date_delivery;
+var object;
+
 function Getdata(e) {
 	e.preventDefault();
 	document.getElementById("Accept_food_msg").textContent = "";
-	var cust_name = document.getElementById("uname").value;
-	var select_location = document.getElementById("location1").value;
-	var select_food = document.getElementById("food_type").value;
-	var date_delivery = document.getElementById("data_order").value;
+	cust_name = document.getElementById("uname").value;
+	select_location = document.getElementById("location1").value;
+	select_food = document.getElementById("food_type").value;
+	date_delivery = document.getElementById("data_order").value;
+	// export default { cust_name, select_location, select_food, date_delivery };
+	object = {
+		customer: cust_name,
+		location: select_location,
+		food: select_food,
+		date: date_delivery,
+	};
+
+	// export default object;
+
+	console.log(object);
+	var json_data = JSON.stringify(object);
+
+	localStorage.setItem("user_data", json_data);
 
 	if (
 		cust_name.length == 0 ||
@@ -23,10 +43,13 @@ function Getdata(e) {
 		document.getElementById("Accept_food_msg").innerHTML =
 			`Thank You! for Your Order` + `<p>&#128522</p>`;
 
-		var cust_name = (document.getElementById("uname").value = "");
-		var select_location = (document.getElementById("location1").value = "");
-		var select_food = (document.getElementById("food_type").value = "");
-		var date_delivery = (document.getElementById("data_order").value = "");
+		cust_name = document.getElementById("uname").value = "";
+		select_location = document.getElementById("location1").value = "";
+		select_food = document.getElementById("food_type").value = "";
+		date_delivery = document.getElementById("data_order").value = "";
+
+		location.href =
+			"http://127.0.0.1:5500/project-2-FoodXprs_clone/plan-my-meal.html";
 	}
 }
 
